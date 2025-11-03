8efc6dd1313f80530291332f148379243b70d81f
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-11-03 15:35:13 -0800

    implement draggable thought bubbles

--- Scripts/MenuComponents/Thought.cs
+++ Scripts/MenuComponents/Thought.cs
@@ -3,15 +3,21 @@ using System;
 
 public class Thought : Control
 {
-    public const float DRAG_DELAY = 0.5f;
+    public const float STICK_AMOUNT = 0.25f;
+    public const float RETURN_TIME = 0.65f;
+
+    private ControlScaler scaler;
+    private Tween tweener;
 
     public bool IsHovered { get; private set; } = false;
     public bool IsHeld { get; private set; } = false;
-    private ControlScaler scaler;
+    private Vector2 OriginalPosition = Vector2.Zero;
 
     public override void _Ready()
     {
         scaler = GetNode<ControlScaler>("ScaleHelper");
+        tweener = GetNode<Tween>("Translation");
+
         Connect(SignalNames.MouseEntered, this, nameof(OnMouseEnter));
         Connect(SignalNames.MouseExited, this, nameof(OnMouseExit));
         Connect(SignalNames.ButtonDown, this, nameof(OnButtonDown));
@@ -20,19 +26,27 @@ public class Thought : Control
 
     public override void _Process(float delta)
     {
-        if (IsHeld) GD.Print("pretend its moving now");
+        if (!IsHeld) return;
+
+        RectPosition = RectPosition.LinearInterpolate(GetPositionFromMouse(), STICK_AMOUNT);
     }
 
     private void OnButtonUp()
     {
         IsHeld = false;
         scaler.ScaleToDefault();
+
+        tweener.InterpolateProperty(this, PropertyNames.RectPosition, RectPosition, OriginalPosition, RETURN_TIME, Tween.TransitionType.Back, Tween.EaseType.InOut);
+        tweener.Start();
     }
 
     private void OnButtonDown()
     {
         IsHeld = true;
         scaler.Scale(0.95f);
+
+        if (!tweener.IsActive()) OriginalPosition = RectPosition;
+        tweener.StopAll();
     }
 
     private void OnMouseEnter()
@@ -49,5 +63,11 @@ public class Thought : Control
 
         IsHovered = false;
         scaler.ScaleToDefault();
+    }
+
+    private Vector2 GetPositionFromMouse()
+    {
+        Vector2 mousePos = GetViewport().GetMousePosition();
+        return mousePos - RectSize / 2;
     }
 }
