78587740d014899e570f9894f3237bb8e65ee412
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-11-03 15:58:27 -0800

    implement movement on thought bubbles

--- Scripts/MenuComponents/Thought.cs
+++ Scripts/MenuComponents/Thought.cs
@@ -3,32 +3,59 @@ using System;
 
 public class Thought : Control
 {
-    public const float STICK_AMOUNT = 0.25f;
-    public const float RETURN_TIME = 0.65f;
+    private const float MIN_SPAWN_VELOCITY = 50;
+    private const float MAX_SPAWN_VELOCITY = 200;
+    private const float STICK_AMOUNT = 0.25f;
+    private const float RETURN_TIME = 0.75f;
 
     private ControlScaler scaler;
     private Tween tweener;
+    private Label text;
 
     public bool IsHovered { get; private set; } = false;
     public bool IsHeld { get; private set; } = false;
+    public Vector2 Velocity { get; private set; } = Vector2.Zero;
     private Vector2 OriginalPosition = Vector2.Zero;
 
     public override void _Ready()
     {
         scaler = GetNode<ControlScaler>("ScaleHelper");
-        tweener = GetNode<Tween>("Translation");
+        tweener = GetNode<Tween>("Tweener");
+        text = GetNode<Label>("Text");
 
         Connect(SignalNames.MouseEntered, this, nameof(OnMouseEnter));
         Connect(SignalNames.MouseExited, this, nameof(OnMouseExit));
         Connect(SignalNames.ButtonDown, this, nameof(OnButtonDown));
         Connect(SignalNames.ButtonUp, this, nameof(OnButtonUp));
+
+        // TODO URGENT: MOVE THIS TO SOMEWHERE ELSE SO IT DOESN'T KEEP ON RESEEDING, FOR NOW THIS IS FINE FOR TESTING MAIN MENU
+        GD.Randomize();
+
+        Velocity = new Vector2((float)GD.RandRange(-1, 1), (float)GD.RandRange(-1, 1)).Normalized() * (float)GD.RandRange(MIN_SPAWN_VELOCITY, MAX_SPAWN_VELOCITY);
     }
 
     public override void _Process(float delta)
     {
-        if (!IsHeld) return;
+        if (tweener.IsActive()) return;
 
-        RectPosition = RectPosition.LinearInterpolate(GetPositionFromMouse(), STICK_AMOUNT);
+        if (IsHeld)
+        {
+            RectPosition = RectPosition.LinearInterpolate(GetViewport().GetMousePosition() - RectSize / 2, STICK_AMOUNT);
+        }
+        else
+        {
+            RectPosition += Velocity * delta;
+        }
+    }
+
+    public void SetText(string newText)
+    {
+        text.Text = newText;
+    }
+
+    public void SetSpeed(float newSpeed)
+    {
+        Velocity = Velocity.Normalized() * newSpeed;
     }
 
     private void OnButtonUp()
@@ -63,11 +90,5 @@ public class Thought : Control
 
         IsHovered = false;
         scaler.ScaleToDefault();
-    }
-
-    private Vector2 GetPositionFromMouse()
-    {
-        Vector2 mousePos = GetViewport().GetMousePosition();
-        return mousePos - RectSize / 2;
     }
 }
