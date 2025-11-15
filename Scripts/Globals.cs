e2edea036edcbb6b65811132a7898b640e388c99
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-11-14 22:20:51 -0800

    move prompt text and answer to globals file

--- Scripts/Globals.cs
+++ Scripts/Globals.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public class Globals : Node
@@ -5,4 +6,37 @@ public class Globals : Node
     public static float MasterVolume = 1;
     public static float SFXVolume = 1;
     public static float MusicVolume = 1;
+
+    // WORD BANK
+    public static readonly string[][] WORD_BANK =
+    {
+        new[]{"", ""},
+        new[]{"", ""},
+        new[]{"", ""},
+        new[]{"", ""},
+        new[]{"", ""},
+        new[]{"", ""}
+    };
+
+    // DIALOGUE SEQUENCES
+    public static readonly Tuple<string, string>[] DIALOGUE_KEY =
+    {
+        new Tuple<string, string>("Welcome to RucRonalds, What can I get for you today?", "One Big Ruc Please"),
+        new Tuple<string, string>("Would you like anything else?", "A Box of RucNuggets"),
+        new Tuple<string, string>("Would you like fries with that?", "Yes/Sure"),
+        new Tuple<string, string>("How about soda?", "Yes/Sure"),
+        new Tuple<string, string>("That would be $6.90 please.", "5 + 2"), // Note: All text options can't add over $6.90
+        new Tuple<string, string>("Would you like change?", "Yes/No")
+    };
+    public static readonly string[] GOOD_END_SEQUENCE =
+    {
+        "One Big Ruc, RucNuggets, and fries with soda coming up.",
+        "Please wait one moment, your order will be ready soon."
+    };
+    public static readonly string[] BAD_END_SEQUENCE =
+    {
+        "I'm sorry but you're holding up the line",
+        "Could you please move aside for the other customers?"
+    };
+    public static readonly string ERROR_TEXT = "Sorry I couldn't quite understand, could you repeat that?";
 }
--- Scripts/MenuComponents/Prompt.cs
+++ Scripts/MenuComponents/Prompt.cs
@@ -4,28 +4,6 @@ using System.Linq;
 
 public class Prompt : Label
 {
-    // DIALOGUE SEQUENCES
-    private readonly string[] DIALOGUE_SEQUENCE =
-    {
-        "Welcome to RucRonalds, What can I get for you today?", // One Big Ruc Please
-        "Would you like anything else?", // A Box of RucNuggets
-        "Would you like fries with that?", // Yes
-        "How about soda?", // Yes
-        "That would be $6.90 please.", // Value > 6.90
-        "Would you like change?" // Yes/No both work
-    };
-    private readonly string[] GOOD_END_SEQUENCE =
-    {
-        "One Big Ruc, RucNuggets, and fries with soda coming up.",
-        "Please wait one moment, your order will be ready soon."
-    };
-    private readonly string[] BAD_END_SEQUENCE =
-    {
-        "I'm sorry but you're holding up the line",
-        "Could you please move aside for the other customers?"
-    };
-    private readonly string ERROR_TEXT = "Sorry I couldn't quite understand, could you repeat that?";
-
     private const float CRAWL_TIME = 1f;
 
     [Signal] public delegate void FinishCrawl();
@@ -40,23 +18,22 @@ public class Prompt : Label
 
     public void NextDialogue()
     {
-        if (dialogueIndex >= DIALOGUE_SEQUENCE.Length)
+        if (dialogueIndex >= Globals.DIALOGUE_KEY.Length)
         {
             EmitSignal(nameof(OutOfDialogue));
             return;
         }
         PercentVisible = 0;
-        Text = DIALOGUE_SEQUENCE[dialogueIndex];
+        Text = Globals.DIALOGUE_KEY[dialogueIndex].Item1;
         dialogueIndex++;
 
-        // TODO: Maybe add some sfx when text is being added? do much later
         PlayCrawl();
     }
 
     public void ErrorDialogue()
     {
         PercentVisible = 0;
-        Text = ERROR_TEXT;
+        Text = Globals.ERROR_TEXT;
         PlayCrawl();
     }
 
@@ -68,6 +45,7 @@ public class Prompt : Label
 
     private void PlayCrawl()
     {
+        // TODO: Maybe add some sfx when text is being added? do much later
         var crawl = CreateTween();
         crawl.TweenProperty(this, PropertyNames.PercentVisible, 1, CRAWL_TIME);
         crawl.Play();
