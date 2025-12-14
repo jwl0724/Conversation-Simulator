9140c07a5954c8fcf16839577de35dd67d81865b
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-12-13 18:20:10 -0800

    create pulse effect nearing end of timer

--- Scripts/GameComponents/TimerBar.cs
+++ Scripts/GameComponents/TimerBar.cs
@@ -1,26 +1,70 @@
 using Godot;
-using System;
 
 public class TimerBar : Control
 {
-    public Timer Timer { get; private set; }
+    private const float maxPulseFactor = 0.015f;
+    private const float maxPulseSpeed = 20;
+    private const float pulseCurveAmount = 1.5f;
+    private const float colorTransTime = 0.1f;
+    private readonly float vfxStartTime = Globals.TIME_LIMIT / 2;
+    [Export] private Color flashColor;
+
+    private Tween tween;
+    private Timer timer;
     private ProgressBar bar;
+    private Node2D barWrapper; // Needed cause animation stuttery without it
+
+    private float theta = 0;
 
     public override void _Ready()
     {
-        Timer = GetNode<Timer>("Timer");
-        bar = GetNode<ProgressBar>("ProgressBar");
+        timer = GetNode<Timer>("Timer");
+        tween = GetNode<Tween>("Tween");
+        barWrapper = GetNode<Node2D>("Node2D");
+        bar = barWrapper.GetNode<ProgressBar>("ProgressBar");
 
-        Timer.WaitTime = Globals.TIME_LIMIT;
+        timer.WaitTime = Globals.TIME_LIMIT;
         bar.MaxValue = Globals.TIME_LIMIT;
         bar.Value = Globals.TIME_LIMIT;
     }
 
-    public override void _Process(float delta)
+    public void ConnectToTimeout(Node node, string methodName)
     {
-        if (Timer.IsStopped() || Timer.Paused) return;
-        bar.Value = Timer.TimeLeft;
+        timer.Connect(SignalNames.Timeout, node, methodName);
     }
 
-    // TODO: Add flashing red effect when running low on time and pulse effect?
+    public void ConnectTimerToSignal(Node connectingTo, string signalName, string timerMethod)
+    {
+        connectingTo.Connect(signalName, timer, timerMethod);
+    }
+
+    public void Start()
+    {
+        timer.Start();
+    }
+
+    public void Stop()
+    {
+        timer.Stop();
+
+        // TODO: Change the color to the default color it isn't due to flashing effect
+        theta = 0;
+        barWrapper.Scale = Vector2.One;
+    }
+
+    public override void _Process(float delta)
+    {
+        if (timer.IsStopped() || timer.Paused) return;
+        bar.Value = timer.TimeLeft;
+
+        // TODO: Add flashing red effect when running low on time and pulse effect?
+        if (timer.TimeLeft > vfxStartTime) return;
+
+        float timeLeftRatio = (vfxStartTime - timer.TimeLeft) / vfxStartTime;
+
+        // Pulse effect
+        barWrapper.Scale = Vector2.One * maxPulseFactor * Mathf.Sin(theta) + Vector2.One;
+        theta += delta * maxPulseSpeed * Mathf.Ease(timeLeftRatio, pulseCurveAmount);
+        if (theta > Mathf.Pi * 2) theta = 0;
+    }
 }
